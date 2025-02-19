978770c1239185cc7b3e1cf7ea1c05716019432d
Nestor Fabio Gomez Toro <neyblue15@hotmail.com>
2025-02-19 18:37:30 -0500

    refactor: migrate FunctionalView to ClassView

--- patients/views.py
+++ patients/views.py
@@ -1,6 +1,7 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import status
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 
 from .serializers import PatientSerializer
 from .models import Patient
@@ -10,18 +11,9 @@ from .models import Patient
 # POST  /api/patients => create_patient
 
 
-@api_view(["GET", "POST"])
-def list_patients(request):
-    if request.method == "GET":
-        patients = Patient.objects.all()
-        serializer = PatientSerializer(patients, many=True)
-        return Response(serializer.data)
-
-    if request.method == "POST":
-        serializer = PatientSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_201_CREATED)
+class ListPatientsView(ListCreateAPIView):
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
 
 
 # GET  /api/patients/pk => Detalle
@@ -29,20 +21,6 @@ def list_patients(request):
 # DELETE  /api/patients/pk => Borrar
 
 
-@api_view(["GET", "PUT", "DELETE"])
-def detail_patient(request, pk):
-    try:
-        patient = Patient.objects.get(id=pk)
-    except Patient.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-    if request.method == "GET":
-        serializer = PatientSerializer(patient)
-        return Response(serializer.data)
-    if request.method == "PUT":
-        serializer = PatientSerializer(patient, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    if request.method == "DELETE":
-        patient.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class DetailPatientView(RetrieveUpdateDestroyAPIView):
+    serializer_class = PatientSerializer
+    queryset = Patient.objects.all()
